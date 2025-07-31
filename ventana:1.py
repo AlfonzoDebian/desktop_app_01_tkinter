2a63e84189ce24dee1f160b97e3f8d3c073dc927
AlfonzoDebian <luismarquez@colegioguanenta.edu.co>
2025-07-31 15:47:56 -0500

    desk

---
+++ ventana:1.py
@@ -0,0 +1,27 @@
+# importar libreria.
+from tkinter import *
+
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Funciones de la app
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Ventana principal de app
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+#se declara una variable  venetana principal
+
+ventana_principal = Tk()
+
+#titulo
+ventana_principal.title("XDebian")
+
+# Tamaño
+ventana_principal.geometry("300x300")
+
+# Iniciar programa
+ventana_principal.mainloop()
